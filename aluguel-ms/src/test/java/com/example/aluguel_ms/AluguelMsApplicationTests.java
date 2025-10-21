d09be822fbdc0adc88b44d7d6f5f949f7f976f9c
Raul Rodrigues <91432196+RaulRJ5@users.noreply.github.com>
2025-10-21 20:02:05 -0300

    Fazendo testes JUnit

--- aluguel-ms/src/test/java/com/example/aluguel_ms/AluguelMsApplicationTests.java
+++ aluguel-ms/src/test/java/com/example/aluguel_ms/AluguelMsApplicationTests.java
@@ -1,13 +1,74 @@
-package com.example.aluguel_ms;
+package com.example.aluguelms.service;
 
+import com.example.aluguelms.model.Funcionario;
+import com.example.aluguelms.repository.FuncionarioRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
-class AluguelMsApplicationTests {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
+class FuncionarioServiceTest {
+
+    @Mock
+    private FuncionarioRepository repository;
+
+    @InjectMocks
+    private FuncionarioService service;
+
+    @Test
+    void testCriarFuncionario() {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Raul");
+        funcionario.setMatricula("2025");
+
+        when(repository.save(funcionario)).thenReturn(funcionario);
+
+        Funcionario resultado = service.criarFuncionario(funcionario);
+
+        assertNotNull(resultado);
+        assertEquals("Raul", resultado.getNome());
+        verify(repository, times(1)).save(funcionario);
+    }
+
+    @Test
+    void testListarTodos() {
+        Funcionario func1 = new Funcionario();
+        func1.setNome("Edson");
+        
+        Funcionario func2 = new Funcionario();
+        func2.setNome("Pedro");
+
+        List<Funcionario> lista = Arrays.asList(func1, func2);
+        when(repository.findAll()).thenReturn(lista);
+		
+        List<Funcionario> resultado = service.listarTodos();
+
+        assertEquals(2, resultado.size());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void testBuscarPorMatricula_Encontrado() {
+		
+        Funcionario funcionario = new Funcionario();
+        funcionario.setMatricula("445");
+        funcionario.setNome("Lucca");
+
+        when(repository.findByMatricula("445")).thenReturn(Optional.of(funcionario));
+
+        Optional<Funcionario> resultado = service.buscarPorMatricula("445");
+
+        assertTrue(resultado.isPresent());
+        assertEquals("Lucca", resultado.get().getNome());
+        verify(repository, times(1)).findByMatricula("445");
+    }
 }
