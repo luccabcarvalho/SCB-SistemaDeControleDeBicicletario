a6c4281403b9ebb4fa3434700866ea4eda2064f2
Edson Luiz <100296179+EdsonLuizz@users.noreply.github.com>
2025-10-21 23:27:34 -0300

    Update FuncionarioController.java

--- aluguel-ms/src/main/java/com/example/aluguel_ms/controller/FuncionarioController.java
+++ aluguel-ms/src/main/java/com/example/aluguel_ms/controller/FuncionarioController.java
@@ -4,6 +4,7 @@ import com.example.aluguel_ms.model.Funcionario;
 import com.example.aluguel_ms.service.FuncionarioService;
 
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
